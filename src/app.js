0e9f5e025fc700d23d1a4ecb6bcb8513ce330243
NirmalMudaliar <nmudaliar@valorx.com>
2025-07-20 13:35:34 +0530

    chore: initial project setup

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+const PORT = 7777;
+
+app.listen(PORT, () => {
+  console.log(`Server is successfully listening on port ${PORT}`);
+});
