4bda03b96b3153fb36e602f008e1d1786f4668ed
NirmalMudaliar <nmudaliar@valorx.com>
2025-07-20 16:45:13 +0530

    feat(src): add logic to connect to db

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const { connectToDb } = require('./config/database');
+const { User } = require('./models/user')
 
 const app = express();
 const PORT = 7777;
 
-app.listen(PORT, () => {
-  console.log(`Server is successfully listening on port ${PORT}`);
-});
+connectToDb()
+  .then(() => {
+    console.log('Database connection established');
+
+    app.listen(PORT, () => {
+      console.log(`Server is successfully listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error occured while connecting to database: ', error);
+  })
+
