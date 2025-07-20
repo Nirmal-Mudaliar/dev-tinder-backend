099b788c91cf424b08518edf2358c4c865201694
NirmalMudaliar <nmudaliar@valorx.com>
2025-07-20 17:47:31 +0530

    feat(src): add logic to create get users and user by emailId apis

--- src/app.js
+++ src/app.js
@@ -5,6 +5,39 @@ const { User } = require('./models/user')
 const app = express();
 const PORT = 7777;
 
+app.use(express.json());
+
+app.post('/signup', async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send('User added successfully');
+  }
+  catch (err) {
+    res.status(400).send('Error occured while saving the user: ');
+  }
+});
+
+app.get('/user', async (req, res) => {
+  try {
+    const users = await User.find({ emailId: req.body.emailId });
+    res.send(users[0]);
+  }
+  catch (error) {
+    res.status(400).send("Something went wrong in fetchig user by email id: ");
+  }
+});
+
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  }
+  catch (error) {
+    res.status(400).send("Something went wrong in fetching all the users");
+  }
+})
+
 connectToDb()
   .then(() => {
     console.log('Database connection established');
